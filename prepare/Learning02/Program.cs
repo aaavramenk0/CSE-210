9f3fff2a72f8e6d38597c6e956648070409dc5c0
Aleksandr <alex.avramenko2004@gmail.com>
2023-03-02 18:47:51 -0500

    Learning activities week 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Social media manager";
+        job1._company = "Kelly Landscapes Management";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+        job1.DisplayJob();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Lawn care technician";
+        job2._company = "Kelly Landscapes Management";
+        job2._startYear = 2023;
+        job2._endYear = 2023;
+        job2.DisplayJob();
+
+        Resume myResume = new Resume();
+        myResume._name = "Oleksandr Avramenko";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayJobs();
     }
 }
