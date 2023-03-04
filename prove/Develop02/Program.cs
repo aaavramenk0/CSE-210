ebf66c65bec1f78cbef808fc2251d93e22027b05
Aleksandr <alex.avramenko2004@gmail.com>
2023-03-04 14:57:01 -0500

    Developer exceed requirements

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -3,6 +3,18 @@ using System.Collections.Generic;
 
 class Program
 {
+    // EXCEED REQUIREMENTS
+    // I add Date to file name while notes are loading into file
+    // It can be useful in case we connect database and load a lot of notes on different dates
+    //
+    // Also it can be possible to save name of the user and create Journal for couples or groups where 
+    // people will save their notes and in the file it will be shown: Date, Time, Name, Prompt, User answer
+    // 
+    // As an addition to previous It can be possible to add comments to the notes. It can be one more item
+    // in intro message and load all previous notes and give oportunity to comment that
+    //
+    // Also it can be possible to give users oportunity to edit their notes.
+    
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the Journal Program!");
