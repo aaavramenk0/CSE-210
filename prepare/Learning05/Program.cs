7c743354b70968eb6e8159bc1b798edd6befc085
Aleksandr <alex.avramenko2004@gmail.com>
2023-04-01 19:11:44 -0400

    Learning 5 week

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("red", 5);
+        Rectangle rectangle = new Rectangle(12, 3, "green");
+        Circle circle = new Circle(4, "yellow");
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
